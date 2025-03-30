0989ac9817e7c2bdcf73d115b809a934e69b4289
장연우 <jangyw0927@naver.com>
2025-03-30 23:46:02 +0900

    // 바닥 제거(기존 모델과 중복 충돌)

--- src/components/Gallery3D_Walkable.jsx
+++ src/components/Gallery3D_Walkable.jsx
@@ -141,11 +141,15 @@ export default function Gallery3D_Walkable() {
         <Physics gravity={[0, -9.81, 0]}>
           {/* 바닥 */}
           <RigidBody type="fixed" colliders="cuboid">
-  <mesh rotation={[-Math.PI / 2, 0, 0]} position={[0, -1, 0]} receiveShadow>
-    <planeGeometry args={[300, 300]} />
-    <meshStandardMaterial color="#e6e6e6" />
-  </mesh>
-</RigidBody>
+            <mesh
+              rotation={[-Math.PI / 2, 0, 0]}
+              position={[0, -1, 0]}
+              receiveShadow
+            >
+              <planeGeometry args={[300, 300]} />
+              <meshStandardMaterial color="#e6e6e6" />
+            </mesh>
+          </RigidBody>
 
           {/* 모델 */}
           <GalleryModel scale={2} />
