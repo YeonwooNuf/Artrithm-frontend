8926908d57e9baf25909ed3c575d748890cacc35
장연우 <jangyw0927@naver.com>
2025-04-21 14:34:48 +0900

    // 선택한 id값의 작품 배열 전시

--- src/components/Gallery3D/Gallery3D.jsx
+++ src/components/Gallery3D/Gallery3D.jsx
@@ -3,6 +3,7 @@ import { Canvas } from "@react-three/fiber";
 import { Environment, PointerLockControls } from "@react-three/drei";
 import { Physics, RigidBody } from "@react-three/rapier";
 import * as THREE from "three";
+import { useLocation } from "react-router-dom";
 
 import Player from "./Player";
 import GalleryModel from "./GalleryModel";
@@ -10,6 +11,9 @@ import Painting from "./Painting";
 import "./Gallery3D.css";
 
 export default function Gallery3D() {
+  const location = useLocation();
+  const works = location.state?.works || [];
+
   return (
     <div className="gallery3d-wrapper fade-in">
       <h2 className="gallery-title">3D 작품 전시관</h2>
@@ -36,6 +40,14 @@ export default function Gallery3D() {
             <Painting position={[-5, 2, 3.01]} imageUrl="/art1.png" title="작품 1" />
             <Painting position={[0, 2, 3.01]} imageUrl="/art2.jpeg" title="작품 2" />
             <Painting position={[5, 2, 3.01]} imageUrl="/art3.jpeg" title="작품 3" />
+            {works.map((art, idx) => (
+              <Painting
+                key={art.id}
+                position={[-20 + idx * 5, 2, 3.01]}
+                imageUrl={art.src}
+                title={art.title}
+              />
+            ))}
           </Physics>
         </Canvas>
 
