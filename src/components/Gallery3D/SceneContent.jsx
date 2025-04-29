cebdead14c771467c3822e3d4370daa197f3ff84
장연우 <jangyw0927@naver.com>
2025-04-29 18:18:49 +0900

    // 전시관 전체 밝기 정도 수정

--- src/components/Gallery3D/SceneContent.jsx
+++ src/components/Gallery3D/SceneContent.jsx
@@ -14,7 +14,7 @@ export default function SceneContent({ layout, works, theme, focusedId, infoId }
 
   return (
     <>
-      <ambientLight intensity={0.5} />
+      <ambientLight intensity={0.4} /> {/* 전시관 모델 전체 밝기 정도 */}
       <Physics gravity={[0, -9.81, 0]}>
         <RigidBody type="fixed" colliders="cuboid">
           <mesh rotation={[-Math.PI / 2, 0, 0]} position={[0, -1, 0]} receiveShadow>
