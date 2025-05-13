72afbae92647f69b8256f93b0c9dc4ca0fc79140
장연우 <jangyw0927@naver.com>
2025-05-13 12:20:30 +0900

    // 백엔드 경로 명시

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8080',
+        changeOrigin: true,
+        secure: false,
+      },
+      '/uploads': {
+        target: 'http://localhost:8080', // ✅ 이미지도 백엔드에서
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
+});
