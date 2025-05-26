895bce6aefd5330d6b7ab3f062ce5388de408d76
장연우 <jangyw0927@naver.com>
2025-05-26 12:46:08 +0900

    // 관리자용 사이드바 추가

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,16 +1,24 @@
 import React from "react";
 import Header from "../Header/Header";
 import Footer from "../Footer/Footer";
-import { Outlet } from "react-router-dom";
+import AdminSidebar from "../Admin/AdminSidebar";
+import { Outlet, useLocation } from "react-router-dom";
 
 const Layout = ({ user, setUser }) => {
+  const isAdmin = user?.role === "ADMIN";
+  const location = useLocation();
+  const isMyPage = location.pathname === "/mypage";
+
   return (
-    <div className="w-full h-full">
-      <Header user={user} setUser={setUser} />
-      <main>
-        <Outlet />
-      </main>
-      <Footer />
+    <div className="w-full h-full flex">
+      {isAdmin && isMyPage && <AdminSidebar />}
+      <div className="flex-1">
+        <Header user={user} setUser={setUser} />
+        <main>
+          <Outlet />
+        </main>
+        <Footer />
+      </div>
     </div>
   );
 };
