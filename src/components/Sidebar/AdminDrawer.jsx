af07e1fcbd4c3bca22989c6efd24c3305c385069
jinhakim01 <jinistar2222@gmail.com>
2025-06-16 16:19:47 +0900

    // 관리자 경매등록

--- src/components/Sidebar/AdminDrawer.jsx
+++ src/components/Sidebar/AdminDrawer.jsx
@@ -5,13 +5,26 @@ import "./AdminDrawer.css";
 export default function AdminDrawer({ onClose, isOpen }) {
   return (
     <div className={`admin-drawer ${isOpen ? "open" : ""}`}>
-      <button className="close-btn" onClick={onClose}>×</button>
+      <button className="close-btn" onClick={onClose}>
+        ×
+      </button>
       <h3>관리자 메뉴</h3>
       <ul>
-        <li><Link to="/admin/upload-masterpiece">명화 전시 업로드</Link></li>
-        <li><Link to="/admin/artists/new">작가 등록</Link></li>
-        <li><Link to="/admin/upload-description">작품 설명 업로드</Link></li>
-        <li><Link to="/admin/approve-promotion">작가 승인 요청 목록</Link></li>
+        <li>
+          <Link to="/admin/upload-masterpiece">명화 전시 업로드</Link>
+        </li>
+        <li>
+          <Link to="/admin/artists/new">작가 등록</Link>
+        </li>
+        <li>
+          <Link to="/admin/upload-description">작품 설명 업로드</Link>
+        </li>
+        <li>
+          <Link to="/admin/approve-promotion">작가 승인 요청 목록</Link>
+        </li>
+        <li>
+          <Link to="/auction-regist">경매등록</Link>
+        </li>
       </ul>
     </div>
   );
