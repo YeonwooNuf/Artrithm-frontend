51484a7690bfe2680996ddf07d38c4c0fb4fe139
jinhakim01 <jinistar2222@gmail.com>
2025-06-17 14:37:31 +0900

    // 장바구니 아이템 사진 불러오기

--- vite.config.js
+++ vite.config.js
@@ -1,25 +1,26 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: true, // 외부 접근 허용
     proxy: {
       // 🔁 FastAPI (LLM)
-      '/api/artchat': {
-        target: 'http://localhost:8000',
+      "/api/artchat": {
+        target: "http://192.168.0.56:8000",
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api\/artchat/, '/api/artchat'),
+        rewrite: (path) => path.replace(/^\/api\/artchat/, "/api/artchat"),
       },
       // 🔁 Spring Boot 백엔드
-      '/api': {
-        target: 'http://localhost:8080',
+      "/api": {
+        target: "http://192.168.0.56:8080",
         changeOrigin: true,
         secure: false,
       },
       // 🔁 이미지
-      '/uploads': {
-        target: 'http://localhost:8080',
+      "/uploads": {
+        target: "http://192.168.0.56:8080",
         changeOrigin: true,
         secure: false,
       },
