b307b99808968f9ec4b70acc20a241809f487092
장연우 <jangyw0927@naver.com>
2025-07-11 19:20:04 +0900

    // IP 주소 변경

--- vite.config.js
+++ vite.config.js
@@ -5,32 +5,33 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: true, // 외부 접근 허용
+    
     proxy: {
       // FastAPI LLM 챗봇 기능
 
       "/api/artchat": {
-        target: "http://localhost:8000",
+        target: "http://192.168.10.159:8000",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api\/artchat/, "/api/artchat"),
       },
 
       // FastAPI LLM 추천 기능
       "/api/recommend": {
-        target: "http://localhost:8000",
+        target: "http://192.168.10.159:8000",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api\/recommend/, "/api/recommend"),
       },
       // 🔁 Spring Boot 백엔드
 
       "/api": {
-        target: "http://localhost:8080",
+        target: "http://192.168.10.159:8080",
         changeOrigin: true,
         secure: false,
       },
       // 🔁 이미지
 
       "/uploads": {
-        target: "http://localhost:8080",
+        target: "http://192.168.10.159:8080",
         changeOrigin: true,
         secure: false,
       },
